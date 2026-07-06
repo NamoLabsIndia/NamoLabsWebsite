6081531de2a5de29541fda6f7379633497e2eafe
balatharunr <r.balatharun@gmail.com>
2026-07-06 10:42:12 +0530

    add PartnershipPage component with Hero and PartnershipModels

---
+++ app/partnership/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/partnership/Hero";
+import PartnershipModels from "@/components/partnership/PartnershipModels";
+
+export default function PartnershipPage() {
+  return (
+    <>
+      <Hero />
+      <PartnershipModels />
+    </>
+  );
+}
